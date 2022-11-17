fcfd8b3c00498e242e4cde094e723a99fd57ae91
Stone <mfollari@backtrace.io>
2022-11-17 13:54:05 -0500

    add mui

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Grid from '@mui/material/Grid'
+import { Typography } from '@mui/material'
 
 export default function Homepage(): JSX.Element {
   return (
     <div>
-      <p>Homepage duck</p>
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography variant="h6"></Typography>
+        </Grid>
+      </Grid>
     </div>
   )
 }
